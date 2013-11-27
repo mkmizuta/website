efd68a8beb83d9993e681c9d2d1ea71cc477e343
Melinda Mizuta <mkmizuta@gmail.com>
2013-11-27 15:20:47 -0800

    added YAML

--- my_app.rb
+++ my_app.rb
@@ -1,17 +1,49 @@
 require 'sinatra'
+require 'yaml'
 
 class MyApp < Sinatra::Base
   
+  before do
+    @posts = Dir.glob("views/posts/*.erb").map do |post_name|
+    post_name.split("/").last.slice(0..-5)
+    end
+  end
+
   get "/" do 
     erb :index
   end
 
-  get "/contact" do
-    erb :contact
+  get "/about" do
+    erb :about
   end
 
+  get "/portfolio" do
+    erb :portfolio
+  end
+  
   get "/blog/:post_name" do
-    erb "/posts/#{params[:post_name]}".to_sym
+    page = erb("/posts/#{params[:post_name]}".to_sym, layout: false)
+    page_body = page.split("\n\n", 2).last
+    erb page_body
+    # erb "/posts/#{params[:post_name]}".to_sym
+  end
+  
+  def meta_data
+    if @meta_data
+      @meta_data
+    else
+      @meta_data = {}
+      @posts.each do |post|
+        html = erb("/posts/#{post}".to_sym, layout: false)
+        meta = YAML.load(html.split("\n\n", 2).first)
+        @meta_data[post] = meta
+      end 
+      @meta_data
+    end
+  end
+
+  get "/contact" do
+    erb :contact
   end
 end
 
